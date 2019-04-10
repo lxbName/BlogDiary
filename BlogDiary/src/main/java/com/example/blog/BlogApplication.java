351efd5c07f7ea4875c5a8ea76ffcf3fb551bb26
lxbName <45644427+lxbName@users.noreply.github.com>
2019-04-10 08:45:52 +0800

    修改blogApplication文件

--- BlogDiary/src/main/java/com/example/blog/BlogApplication.java
+++ BlogDiary/src/main/java/com/example/blog/BlogApplication.java
@@ -2,8 +2,9 @@ package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BlogApplication {
 
     public static void main(String[] args) {
