e28b874dfb25bec0b392baee171ae7d1155a1b53
lxbName <45644427+lxbName@users.noreply.github.com>
2019-04-12 15:35:05 +0800

    修改调用mapper-xml中的sql

--- BlogDiary/src/main/java/com/example/blog/mapper/Common.java
+++ BlogDiary/src/main/java/com/example/blog/mapper/Common.java
@@ -4,14 +4,15 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 @Mapper
 @Component
 public interface Common{
 
-    //登录查询
-    @Select("select userName from user where id=#{id} and userPassword=#{userPassword}")
     public String login(@Param("id") String id,@Param("userPassword") String userPassword);
+
+    //登录查询
+//    @Select("select userName from user where id=#{id} and userPassword=#{userPassword}")
+//    public String login(@Param("id") String id,@Param("userPassword") String userPassword);
 
 }
--- BlogDiary/src/main/java/com/example/blog/services/CommonService.java
+++ BlogDiary/src/main/java/com/example/blog/services/CommonService.java
@@ -1,6 +1,7 @@
 package com.example.blog.services;
 
 import com.example.blog.mapper.Common;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
